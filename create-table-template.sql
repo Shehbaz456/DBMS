4d27f1714d1aa30fb60d5ca2c58583accbdab214
Shehbaz456 <106955650+Shehbaz456@users.noreply.github.com>
2022-06-15 13:14:25 +0530

    Update and rename create-table-template.sql to Sql_Test.sql

--- create-table-template.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE table_name(  
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    create_time DATETIME COMMENT 'Create Time',
-    update_time DATETIME COMMENT 'Update Time',
-    content VARCHAR(255) COMMENT 'content'
-) DEFAULT CHARSET UTF8 COMMENT 'newTable';
