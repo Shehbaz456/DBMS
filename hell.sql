d77679e12a88e96c26f314e57fc82d80b11e5037
Shehbaz456 <106955650+Shehbaz456@users.noreply.github.com>
2022-07-29 16:18:05 +0530

    Add files via upload

---
+++ hell.sql
@@ -0,0 +1,21 @@
+use test;
+
+CREATE table Best(id int ,Name char(50), department CHAR(34),Dep_id INT);
+
+insert into Best VALUES(1,"Shehbaz","IP",102);
+
+SELECT *FROM Best;
+
+insert into Best VALUES(2,"satvik","CS",103);
+insert into Best VALUES(3,"hello","CS",104);
+insert into Best VALUES(4,"Rahul","CS",105);
+insert into Best VALUES(5,"Jhon","CS",106);
+
+SELECT *from Best;
+
+
+
+
+
+
+
